9324196ec272189437546ee2cbe591b8488a6095
Melvin Kolleh <kollehmelvin62@gmail.com>
2025-09-24 22:17:59 -0500

    Update Flutter web build for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "47d6954cdb0860bf283bedb66cf145aa
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "0b089af078c9ee8508c8a5550f4d3de2",
+"flutter_bootstrap.js": "aa11a0ab39f906dd993ddf98154d6276",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
